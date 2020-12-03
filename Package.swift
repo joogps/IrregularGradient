d66df0410cf10087113ca8e97e3ac1d2e268807d
joogps <joogps@gmail.com>
2020-12-02 23:08:25 -0300

    Adding files

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "IrregularGradient",
+    platforms: [
+        .iOS(.v14)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -21,8 +24,5 @@ let package = Package(
         .target(
             name: "IrregularGradient",
             dependencies: []),
-        .testTarget(
-            name: "IrregularGradientTests",
-            dependencies: ["IrregularGradient"]),
     ]
 )
--- Sources/IrregularGradient/IrregularGradient.swift
+++ Sources/IrregularGradient/IrregularGradient.swift
@@ -1,3 +1,70 @@
-struct IrregularGradient {
-    var text = "Hello, World!"
+//
+//  IrregularGradient.swift
+//  IrregularGradient
+//
+//  Created by João Gabriel Pozzobon dos Santos on 01/12/20.
+//
+
+import SwiftUI
+
+struct IrregularGradient: View {
+    
+    @State var colors: [Color]
+    var backgroundColor: Color
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                ForEach(colors, id: \.self) { color in
+                    Blob(color: color, geometry: geometry)
+                }
+            }.blur(radius: 50)
+            .clipped()
+            .background(backgroundColor)
+        }
+    }
+}
+
+extension View {
+    func irregularGradient(colors: [Color], backgroundColor: Color) -> some View {
+        self.overlay(IrregularGradient(colors: colors, backgroundColor: backgroundColor)).mask(self)
+    }
+}
+
+struct Blob: View {
+    @State var color: Color
+    @State var position: CGPoint = CGPoint(x: 0, y: 0)
+    @State var scale: CGSize = CGSize(width: 1, height: 1)
+    
+    var geometry: GeometryProxy
+    
+    let timer = Timer.publish(every: 5, on: .main, in: .common).autoconnect()
+    
+    var body: some View {
+        Circle()
+            .fill(color)
+            .scaleEffect(scale)
+            .position(position)
+            .opacity(0.85)
+            .animation(.spring(response: 15, dampingFraction: 0.3))
+            .onAppear() {
+                update()
+                withAnimation { update() }
+            }.onReceive(timer) { input in
+                withAnimation { update() }
+            }
+    }
+    
+    func update() {
+        position = CGPoint(x: CGFloat.random(in: 0...geometry.size.width), y: CGFloat.random(in: 0...geometry.size.height))
+        scale = CGSize(width: CGFloat.random(in: 0.25...2), height: CGFloat.random(in: 0.25...2))
+    }
+}
+
+struct IrregularGradient_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Text("Irregular Gradient").multilineTextAlignment(.center).font(.system(size: 72, weight: .bold)).irregularGradient(colors: [.yellow, .red, .orange, .pink, .orange, .pink, .yellow, .red], backgroundColor: .orange)
+        }
+    }
 }
