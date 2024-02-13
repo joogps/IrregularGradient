99e9e4376d6c6d7958a13b11528da47da34a4712
João Gabriel <joogps@gmail.com>
2024-02-13 20:35:07 -0300

    Add visionOS support

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "IrregularGradient",
     platforms: [
-        .iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)
+        .iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6), .visionOS(.v1)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
